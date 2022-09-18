90127b1d0e502da220fe58f0203110c55f0d7046
Harjeet Kumar <harjeet.kumar24@gmail.com>
2022-09-18 12:17:28 +0530

    Update POWXN.java

--- src/medium/POWXN.java
+++ src/medium/POWXN.java
@@ -1,1 +1,35 @@
 // https://leetcode.com/problems/powx-n/
+
+class Solution {
+    public double myPow(double x, int n) {
+        if( x==1.0 ){
+            return 1;
+        }
+        
+        if(n == -2147483648 && x < 0.0){
+            return 1;
+        }
+        if(n == -2147483648 ){
+            return 0;
+        }
+        
+        if(n == 0 ){
+            return 1;
+        }
+        if(n < 0){
+            int newN= n * -1;
+            double temp = myPow(x,newN);
+            return 1 / temp;
+        }
+        
+        if(n % 2 == 0){
+            double res = myPow(x,n/2);
+            return  res * res;
+        }
+        
+        double res = myPow(x,n/2);
+        
+        return x * res * res;
+        
+    }
+}
